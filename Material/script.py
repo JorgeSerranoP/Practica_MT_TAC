e194721f4183d5c40e2715b35fed36d230cf3864
Javier Cruz del Valle <100383156@alumnos.uc3m.es>
2022-04-01 17:31:20 +0200

    Entrega 1

--- Material/script.py
+++ Material/script.py
@@ -1,7 +1,7 @@
 import sys
 import xml.dom.minidom
 
-x = "1. Suma de enteros base UNO\SU-MTDeterminista2C.jff"
+x = ""
 doc = xml.dom.minidom.parse(x)
 
 states = {}
@@ -25,8 +25,7 @@ print(f"accept: {','.join(final)}")
 
 
 for tr in doc.getElementsByTagName("transition"):
-    # <from>5</from>
-    # <to>5</to>
+
     fr = tr.getElementsByTagName("from")[0].firstChild.nodeValue
     to = tr.getElementsByTagName("to")[0].firstChild.nodeValue
 
@@ -34,17 +33,16 @@ for tr in doc.getElementsByTagName("transition"):
     write = {}
     move = {}
 
-    # <read tape="1">1</read>
     for e in tr.getElementsByTagName("read"):
         tape = e.getAttribute("tape") or 0
         val = e.firstChild and e.firstChild.nodeValue
         read[int(tape)] = val
-    # <write tape="1">1</write>
+
     for e in tr.getElementsByTagName("write"):
         tape = e.getAttribute("tape") or 0
         val = e.firstChild and e.firstChild.nodeValue
         write[int(tape)] = val
-    # <move tape="1">R</move>
+
     for e in tr.getElementsByTagName("move"):
         tape = e.getAttribute("tape") or 0
         val = e.firstChild and e.firstChild.nodeValue
@@ -55,6 +53,7 @@ for tr in doc.getElementsByTagName("transition"):
         "R": ">",
         "S": "-"
     }
+
     rds = ",".join(str(v or "_") for k, v in sorted(read.items()))
     wrs = ",".join(str(v or "_") for k, v in sorted(write.items()))
     mvs = ",".join(mconv[v] for k, v in sorted(move.items()))
